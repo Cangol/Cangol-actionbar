62b5743824ffdba5e59b5aa7026f827e5ff54916
Cangol <wxw404@gmail.com>
2019-11-20 16:51:08 +0800

    add setFullScreenNoActionBar

--- actionbar/src/main/java/mobi/cangol/mobile/actionbar/ActionBarActivity.java
+++ actionbar/src/main/java/mobi/cangol/mobile/actionbar/ActionBarActivity.java
@@ -311,7 +311,20 @@ public class ActionBarActivity extends AppCompatActivity {
     public void setFullScreen(boolean fullscreen) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             if (fullscreen) {
-                this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                //this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                int option = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                this.getWindow().getDecorView().setSystemUiVisibility(option);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    WindowManager.LayoutParams lp = this.getWindow().getAttributes();
+                    lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+                    this.getWindow().setAttributes(lp);
+                }
             } else {
                 this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
             }
@@ -328,7 +341,40 @@ public class ActionBarActivity extends AppCompatActivity {
             this.getWindow().getDecorView().setSystemUiVisibility(newVisibility);
         }
     }
+    public void setFullScreenNoActionBar(boolean fullscreen) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (fullscreen) {
+                setActionbarShow(true);
+                findViewById(R.id.container_view).setFitsSystemWindows(true);
+                int option = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                this.getWindow().getDecorView().setSystemUiVisibility(option);
 
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    WindowManager.LayoutParams lp = this.getWindow().getAttributes();
+                    lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+                    this.getWindow().setAttributes(lp);
+                }
+            } else {
+                this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+        } else {
+            int newVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+
+            if (fullscreen) {
+                newVisibility |= View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            }
+            // Set the visibility
+            this.getWindow().getDecorView().setSystemUiVisibility(newVisibility);
+        }
+    }
     /**
      * 是否全屏
      */
