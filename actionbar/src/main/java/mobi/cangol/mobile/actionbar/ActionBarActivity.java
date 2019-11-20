1f7c4b8c5a0f5e22bd6434e52cc58acfb87e76ef
yuminfeng <370983992@qq.com>
2019-11-20 18:57:30 +0800

    *): setFullScreen 保留

--- actionbar/src/main/java/mobi/cangol/mobile/actionbar/ActionBarActivity.java
+++ actionbar/src/main/java/mobi/cangol/mobile/actionbar/ActionBarActivity.java
@@ -325,20 +325,7 @@ public class ActionBarActivity extends AppCompatActivity {
     public void setFullScreen(boolean fullscreen) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             if (fullscreen) {
-                //this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-                int option = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-                this.getWindow().getDecorView().setSystemUiVisibility(option);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    WindowManager.LayoutParams lp = this.getWindow().getAttributes();
-                    lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-                    this.getWindow().setAttributes(lp);
-                }
+                this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
             } else {
                 this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
             }
